5e825c767e5474f43d0813c9c7553def47f4ba39
Andrea Manzo <acvmanzo@gmail.com>
2013-07-26 17:19:08 -0700

    Code for MW test and FDR correction

--- statsdata.py
+++ statsdata.py
@@ -1,4 +1,4 @@
-import statsdata as sd
+import rstatslib as rsl
 import plotdata as pd
 import rpy2.robjects as robjects
 import rpy2.robjects.numpy2ri as rpyn
@@ -6,64 +6,91 @@ import os
 
 KINDLIST = ['wing', 'copsuc', 'copatt1']
 DATAFILE = '/home/andrea/Documents/lab/behavior_ca/courtship/codprac/2013-07_courtship_inprog.csv'
-SHAPFILE = '/home/andrea/Documents/lab/behavior_ca/courtship/codprac/shapfile.txt'
+SHAPFILE = '/home/andrea/Documents/lab/behavior_ca/courtship/codprac/shap.txt'
+MCFILE = '/home/andrea/Documents/lab/behavior_ca/courtship/codprac/mcpvalue.txt'
+CTRL = 'cs'
+
+def createshapfile(shapfile):
+
+    r = robjects.r
+    normsample = r['rnorm'](50)
+    x = rsl.shapirowilk(normsample)
+    with open(shapfile, 'w') as f:
+        f.write('Shapiro-Wilk test results\n')
+        f.write('Condition\tBehavior\tp-value\n')
+        f.write('{0}\t{1}\t{2:.4g}\n'.format('normsample', 'normsample', x[1][0]))
+
+
+def writeshapfile(shapfile, datafile, kind):
+
+    r = robjects.r
+    unlist = r['unlist']
+    kname = kind + 'lat'
+
+    d = pd.dictlat(kind, DATAFILE)
+    for n, v in d.iteritems():
+        v = unlist(v)
+        x = rsl.shapirowilk(v)
+        with open(SHAPFILE, 'a') as f:
+            f.write('{0}\t{1}\t{2:.4g}\n'.format(n, kname, x[1][0]))
+
+
+
+#createshapfile(SHAPFILE)
+#for k in KINDLIST:
+    #writeshapfile(SHAPFILE, DATAFILE, k)
 
 r = robjects.r
-#winglat, copsuclat, copattlat = dictlat(FNAME)
-
-#mwinglat, mcopsuclat, mcopattlat = map(means,[winglat, copsuclat, copattlat])
-##print('python', winglat['cs'])
-
-#rcs_wlat = robjects.FloatVector(winglat['cs'])
-#rnrxi_wlat = robjects.FloatVector(winglat['NrxI'])
-#rnrxiv_wlat = robjects.FloatVector(winglat['NrxIV'])
-
-#rcs_copattlat = robjects.FloatVector(copattlat['cs'])
-#rnrxi_copattlat = robjects.FloatVector(copattlat['NrxI'])
-
-#rcs_copsuclat = robjects.FloatVector(copsuclat['cs'])
-#rnrxi_copsuclat = robjects.FloatVector(copsuclat['NrxI'])
-
-#rnorm = r['rnorm']
-#datanorm = rnorm(50)
-#print(datanorm)
-
-#shaptest = r['shapiro.test']
-#print('cs')
-#print(shaptest(rcs_copattlat))
-#print('norm')
-#print(shaptest(datanorm))
-
-#wilcoxtest = r['wilcox.test']
-#x = wilcoxtest(rcs_copsuclat, rnrxi_copsuclat)
-
-#isatomic = r['is.atomic']
-
-
 unlist = r['unlist']
-with open(SHAPFILE, 'w') as e:
-    e.write('Shapiro-Wilk test results\n')
-    e.write('Condition,Behavior,p-value\n')
+k = KINDLIST[0]
+d = pd.dictlat(k, DATAFILE)
+print(d)
 
-rnorm = r['rnorm']
-ctrl = rnorm(50)
-x = sd.shapirowilk(ctrl)
-with open(SHAPFILE, 'a') as f:
-    f.write('{0},{1},{2:.4g}\n'.format('ctrl', 'ctrl', x[1][0]))
+mwlist = []
+mwdict = {}
 
-for k in KINDLIST:
+for n,v in d.iteritems():
+    #print(n)
+    v, ctrl = map(unlist, [v, d[CTRL]])
+    #v = unlist(v)
+    #ctrl = unlist(d[CTRL])
 
-    #if os.path.exists(SHAPFILE) == False:
-    kname = k + 'lat'
-    d = pd.dictlat(k, DATAFILE)
+    r = robjects.r
+    test = r['wilcox.test']
+    z = test(v, ctrl, alternative="two.sided")
+    #print(z)
 
-    for n, v in d.iteritems():
+    #print(z.rx('p.value'))
+    #z = rsl.mannwhitney(v,ctrl)
+    #z1 = r('z[['statistic']]')
 
-        v = unlist(v)
-        x = sd.shapirowilk(v)
+    #print(n, z[0], z[1], z[2])
+    mwlist.append((n, z.rx('p.value')[0][0]))
+    mwdict[n] = {}
+    mwdict[n]['sigtest'] = z.rx('method')[0][0]
+    mwdict[n]['pval'] = z.rx('p.value')[0][0]
 
-        with open(SHAPFILE, 'a') as f:
-            f.write('{0},{1},{2:.4g}\n'.format(n, kname, x[1][0]))
+#print(mwlist)
+
+a, b = zip(*mwlist)
+#print(a)
+#print(b)
+adjustp = rsl.padjust(list(b), "fdr", len(b))
+#print(adjustp)
+
+c = zip(a, adjustp)
+#print(c)
+print(mwdict)
+for item in c:
+    print(mwdict[item[0]])
+    mwdict[item[0]]['adjpval'] = item[1]
+
+print(mwdict)
+
+with open(MCFILE, 'w') as f:
+    for i, v in mwdict.iteritems():
+        f.write('{0}\t{1}\t{2}\n'.format(i, v['pval'], v['adjpval']))
 
 
-
+#with open(MCFILE, 'w') as f:
+    #f.write('{0}'.format(c[0]))
