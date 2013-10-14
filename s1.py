64a5e9f9af9f9a6aa989d2f91ea4f2fb3235fb25
Andrea Manzo <acvmanzo@gmail.com>
2013-10-13 20:58:18 -0700

    Plotting ROI intensities

--- s1.py
+++ s1.py
@@ -16,6 +16,8 @@ import numpy as np
 from wingsettings import *
 from PIL import Image
 import time
+import matplotlib as mpl
+import time
 
 
 exptfiles = exptfiles(os.path.abspath('.'))
@@ -43,7 +45,7 @@ def window(winlen):
     return(wind)
 
 def getfps(exptdir):
-    info = os.path.basename(exptdir).split('_')
+    info = os.path.basename(os.path.abspath(exptdir)).split('_')
     if info.count('PF24') == 1:
         fps = 24
     if info.count('PF30') == 1:
@@ -56,31 +58,90 @@ def movavg(t, fps, trace):
     ctrace = np.convolve(trace/max(trace), wind, 'same')
     return(ctrace)
 
-def plotextwell(smc, mmc, wellnum):
-    plt.figure()
+def plotextwell(smc, mmc, fps, plotlen):
+    '''
+    plotlen = duration of each subplot, in minutes
+    '''
+    
+    # ======== SET INITIAL FIGURE PROPERTIES =============
+    mpl.rc('axes', linewidth=2)
+
+    # Sets font to Arial and assigns font properties.
+    fontv = mpl.font_manager.FontProperties()
+    fontv.set_size(25)
+    
+    plt.figure(figsize=(20,20), dpi=600)
     smc = smc-np.mean(smc)
     mmc = mmc-np.mean(mmc)
-    plt.plot(smc, 'r', label='side-center')
-    plt.plot(mmc, 'b', label='middle-center')
-    plt.title('Well {0}'.format(wellnum))
+    
+    
+    dur = len(smc)/fps/60.
+    
+    print('dur', dur)
+    numplots = range(np.int(np.ceil(dur/plotlen)))
+    print('numplots', numplots)
+    subplots = (len(numplots), 1)
+    
+    xvals = np.linspace(0, dur, len(smc))
+    plotmax = np.max([np.max(smc), np.max(mmc)])
+    
+    for x in numplots:
+        li = 0 + x*plotlen*60*fps
+        ri = plotlen*60*fps + x*plotlen*60*fps
+        ax = plt.subplot2grid(subplots, (x,0), colspan=1)    
+    
+        plt.plot(xvals[li:ri], smc[li:ri], 'r', lw=2, label='side-center')  
+        plt.plot(xvals[li:ri], mmc[li:ri], 'b', lw=2, label='middle-center')
+
+        xmin = xvals[li]
+        xmax = plotlen*x+plotlen
+        print((xmax-xmin)/4)
+        plt.xlim(xmin, xmax)    
+        plt.ylim(0, 1.2*plotmax)
+        
+        xlist = np.linspace(xmin, xmax, 5)
+        print(xlist)
+        xtime = [time.strftime('%M:%S', time.gmtime(x*60)) for x in xlist]
+        plt.xticks(xlist, xtime, fontproperties=fontv)
+        #ax.xaxis.set_major_locator(mpl.ticker.MaxNLocator(5))
+
+    plt.suptitle('Wing extension', fontsize=30)
     plt.legend()
+
+
+
+def saveextwell(wellnum):
+    
     plt.savefig('wingext_well{0:02d}.png'.format(wellnum))
     plt.close()
 
-def plotextexpt(wells, movsmc, movmmc):
-    for well in range(wells):
+
+def plotextexpt(movsmc, movmmc):
+    for well in range(np.shape(movsmc)[1]):
         smc = movsmc[:, well]
         mmc = movmmc[:, well]
         plotextwell(smc, mmc, well)
-
+        
+def plotextexptconv(exptdir, movsmc, movmmc, conv, convt=3):
+    fps = getfps(exptdir)
+    for well in range(np.shape(movsmc)[1]):
+        smc = movsmc[:,well]
+        mmc = movmmc[:,well]
+        if conv == 'yes':
+            smc, mmc = [movavg(convt, fps, x) for x in [smc, mmc]]
+        
+        plotextwell(smc, mmc, fps)
+        saveextwell(well)
+        
 
 #fps = getfps(exptdir)
 #csmc = movavg(1, fps, movsmc[:,4])
 #cmmc = movavg(1, fps, movmmc[:,4])
 #plotwingext(csmc, cmmc)
-plotextexpt(12, movsmc, movmmc)
 
-
+#plotextexptconv('.', movsmc, movmmc, 1)
+plotextwell(movsmc[:,2], movmmc[:,2], 30, 0.5)
+saveextwell(2)
 
                 
 
