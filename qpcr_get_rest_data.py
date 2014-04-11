dcb0e86894b70ef04bd37757d639adf76ec15760
Andrea Manzo <acvmanzo@gmail.com>
2014-04-11 12:41:28 -0700

    Modified qpcr files.

--- qpcr_get_rest_data.py
+++ qpcr_get_rest_data.py
@@ -5,6 +5,7 @@ import glob
 fnames = glob.glob('*.mht')
 #fname = '/home/andrea/Documents/lab/qRT-PCR/3_analysis/rest/bintnu-GP1.mht'
 
+d = {}
 for fname in fnames:
     with open(fname, 'r') as f:
         
@@ -20,26 +21,32 @@ for fname in fnames:
         #data = list(itertools.takewhile(lambda x: x == '<TD valign=3Dtop align=3D"left">', f))
         #print data
 
-    d = {}
     newdata = []
     for da in data:
-        nd =  da.strip('</TD>\r\n').strip('<TD valign=3Dtop align=3D"left">').strip('<TD valign=3Dtop align=3D"right">')
+        print 'old', da
+        nd =  da.rstrip('</TD>\r\n').replace('<TD valign=3Dtop align=3D"left"> ', '').replace('<TD valign=3Dtop align=3D"right"> ', '')
+        print 'new', nd
         newdata.append(nd)    
-    print 'new', newdata
+    #print 'new', newdata
     #target, result = newdata[13], newdata[20]
     #targetexp, pval = [float(x) for x in [newdata[16],  newdata[19]]]
     #stderr = [float(x) for x in newdata[17].split(' - ')]
     #ci95 = [float(x) for x in newdata[18].split(' - ')]
 
-    target = newdata[13]
-    print target 
+    target, result = [newdata[13], newdata[20]]
+    if result == '&nbsp;':
+        result = 'UNCHANGED'
+    #print target 
     d[target] = {}
-    d[target]['result'] = newdata[20]
+    d[target]['result'] = result
     d[target]['exp'], d[target]['pval'] = [float(x) for x in [newdata[16],  newdata[19]]]
     d[target]['stderr'] = [float(x) for x in newdata[17].split(' - ')]
     d[target]['ci95'] = [float(x) for x in newdata[18].split(' - ')]
 
-    print d
+#print d.keys()
+for k in d.keys():
+    print k
+    print d[k]
     #with open(fname, 'r') as f:
         #for l in f:
             ##print repr(l)
--- qpcr_write_mutdata.py
+++ qpcr_write_mutdata.py
@@ -2,4 +2,19 @@ from libs.qrtpcrlib import batch_write_cqs
 
 batch_write_cqs('.')
 
+DROP TABLE qpcr_mut;
 
+CREATE TABLE qpcr_mut
+(  
+dataid int PRIMARY KEY, 
+exptd date,
+exptid varchar (80),
+well character varying(80),
+target character varying(80),
+content character varying(80),
+sample character varying(80),
+cq real
+);
+
+
+\copy qpcr_mut from '/home/andrea/Documents/lab/qRT-PCR/1_data/2014-0410_allmutdata_fmt.csv' with csv header delimiter as ',';
