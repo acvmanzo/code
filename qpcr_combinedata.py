fab6eb16b6cef83d6e6c41f871c684ddb3ffe27b
Andrea Manzo <acvmanzo@gmail.com>
2014-04-12 11:55:51 -0700

    Updated qpcr code

--- qpcr_combinedata.py
+++ qpcr_combinedata.py
@@ -1,8 +1,17 @@
+
 import os
+import psycopg2
 from libs.qrtpcrlib import combinedata
 
-dirname = os.path.dirname(os.path.abspath('.')) + '/' 
-gfilename =  '2014-0410_allmutdata_fmt.csv'
-gname = dirname + gfilename
 
+DIRNAME = os.path.dirname(os.path.abspath('.')) + '/' 
+GFILENAME =  '2014-0410_allmutdata_fmt.csv'
+gname = DIRNAME + GFILENAME
 combinedata(gname)
+
+conn = psycopg2.connect("dbname=andrea user=andrea")
+cur = conn.cursor()
+
+cur.execute("DROP TABLE qpcr_mut; CREATE TABLE qpcr_mut (dataid int PRIMARY KEY, exptd date, exptid varchar (80), well character varying(80), target character varying(80), content character varying(80), sample character varying(80), cq real );")
+
+cur.execute("\copy qpcr_mut from '{0}' with csv header delimiter as ',';".format(gname))
