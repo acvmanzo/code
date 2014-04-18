fc88a1680272ba92acb109acc789f2c77f8d3114
Andrea Manzo <acvmanzo@gmail.com>
2014-04-18 01:10:37 -0700

    Added convert_wiki_table.py

---
+++ convert_wiki_table.py
@@ -0,0 +1,44 @@
+#! /usr/bin/env python
+
+import os
+import sys
+
+print 'Input filename and delimiter as follows: mytable.txt \t'
+
+oldtable = sys.argv[1]
+delim = sys.argv[2] 
+
+if delim == 'tab' or delim == 't':
+    delimiter = '\t'
+else:
+    delimiter = delim
+
+
+### CONVERT TABLE TO WIKI FORMAT ###
+
+def tabletowiki(oldtable, delimiter):
+    root, ext = os.path.splitext(oldtable)
+    newtable = root + '_wiki' + ext
+    with open(newtable, 'w') as g:
+        with open(oldtable, 'r') as f:
+            for l in f:
+                elist = l.strip('\n').split(delimiter)
+                newelist = '||' + '|| '.join(elist) + '||\n'
+                g.write(newelist)
+
+### CONVERT WIKI TABLE TO DELMITED FORMAT ###
+
+def wikitotable(oldtable, delimiter):
+    print repr(delimiter)
+    root, ext = os.path.splitext(oldtable)
+    newtable = root + '_table' + ext
+    with open(newtable, 'w') as g:
+        with open(oldtable, 'r') as f:
+            for l in f:
+                elist = l.strip('\n').split('||')
+                elist = [x.strip(' ') for x in elist]
+                newline = delimiter.join(elist[1:-1]) + '\n'
+                g.write(newline)
+
+if __name__ == "__main__":
+    wikitotable(oldtable, delimiter)
